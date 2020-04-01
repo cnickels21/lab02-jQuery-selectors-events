fc36e0718f6ccfddac8d1733ff7a55a0f32e2b2d
cnickels21 <cnickels_21@yahoo.com>
2020-04-01 11:16:06 -0500

    constructor, prototype and ajax written in

--- js/app.js
+++ js/app.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function Image(image) {
+    this.image_url = image.image_url;
+    this.title = image.title;
+    this.description = image.description;
+    this.keyword = image.keyword;
+    this.horns = image.horns;
+}
+
+Image.prototype.render = function(container) {
+    let $container = $(container);
+    let $template = $container.find('.photo-template');
+    let $image = $template.clone();
+    $image.removeClass('.photo-template');
+    $image.find('.image-name').text(this.title);
+    $image.find('img.image-display').attr(
+        'src', this.image_url);
+    $image.find('p').text(this.description);
+    $container.append($image);
+}
+
+const ajaxSettings = {
+    method: 'get',
+    dataType: 'json'
+};
+
+$.ajax('data/page-1.json', ajaxSettings).then(function (data) {
+        data.forEach((image) => {
+            let displayImage = new Image(image);
+            displayImage.render('main section');
+        });
+    });
